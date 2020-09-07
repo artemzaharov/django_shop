20198538599aacd1d40970d4487882187c58e95c
Arch <omertapiter@gmail.com>
2020-09-07 14:58:36 +0300

    first commit

---
+++ shop/mainapp/admin.py
@@ -0,0 +1,51 @@
+from django.forms import ModelChoiceField, ModelForm, ValidationError
+from django.contrib import admin
+from .models import *
+
+from PIL import Image
+
+class NotebookAdminForm(ModelForm):
+
+    MIN_RESOLUTION = (400, 400)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['image'].help_text = 'Загрузите изображение с минимальным разрешением {}x{}'.format(
+            *self.MIN_RESOLUTION
+            )
+
+    def clean_image(self):
+        image = self.cleaned_data['image']
+        img = Image.open(image)
+        min_height, min_width = self.MIN_RESOLUTION
+        if img.height < min_height or img.width < min_with:
+            raise ValidationError('Разрешение изображение меньше минимального')
+        return image
+
+
+#if we create a notebook there is no choice to change it to smartphone we need forms
+class NotebookAdmin(admin.ModelAdmin):
+
+    form = NotebookAdminForm
+
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == 'category':
+            return ModelChoiceField(Category.objects.filter(slug='notebooks'))
+        return super().formfield_for_foreignkey(self, db_field, request, **kwargs)
+
+
+class SmartphoneAdmin(admin.ModelAdmin):
+
+    
+
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == 'category':
+            return ModelChoiceField(Category.objects.filter(slug='smartphones'))
+        return super().formfield_for_foreignkey(self, db_field, request, **kwargs)
+
+admin.site.register(Category)
+admin.site.register(Notebook, NotebookAdmin)
+admin.site.register(Smartphone, SmartphoneAdmin)
+admin.site.register(CartProduct)
+admin.site.register(Cart)
+admin.site.register(Customer)
